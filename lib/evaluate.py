fa5de1a106737538e111ab3d20b46b323800df14
Brian Okorn <bokorn@andrew.cmu.edu>
2019-05-02 21:30:33 -0400

    Added new eval code

--- lib/evaluate.py
+++ lib/evaluate.py
@@ -1,46 +1,27 @@
-import cv2
-from tqdm import tqdm
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import pylab
 import numpy as np
 import quat_math as qm
-from generic_pose.utils import to_np, to_var
+from generic_pose.utils import to_np 
 
-import os
 import copy
-import random
 import numpy as np
-from PIL import Image
-import scipy.io as scio
-import scipy.misc
 import numpy.ma as ma
-import math
+
 import torch
-import torch.nn as nn
-import torch.nn.parallel
-import torch.backends.cudnn as cudnn
-import torch.optim as optim
-import torch.utils.data
-import torchvision.datasets as dset
 import torchvision.transforms as transforms
-import torchvision.utils as vutils
-import torch.nn.functional as F
 from torch.autograd import Variable
-from datasets.ycb.dataset import PoseDataset
 from lib.network import PoseNet, PoseRefineNet
-from lib.transformations import euler_matrix, quaternion_matrix, quaternion_from_matrix
+from lib.transformations import quaternion_matrix, quaternion_from_matrix
 
 norm = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 border_list = [-1, 40, 80, 120, 160, 200, 240, 280, 320, 360, 400, 440, 480, 520, 560, 600, 640, 680]
 xmap = np.array([[j for i in range(640)] for j in range(480)])
 ymap = np.array([[i for i in range(640)] for j in range(480)])
 
-def get_bbox(posecnn_rois, idx = 0, img_width=480, img_length=640):
-    rmin = int(posecnn_rois[idx][3]) + 1
-    rmax = int(posecnn_rois[idx][5]) - 1
-    cmin = int(posecnn_rois[idx][2]) + 1
-    cmax = int(posecnn_rois[idx][4]) - 1
+def get_bbox(rois, img_width=480, img_length=640):
+    rmin = int(rois[3]) + 1
+    rmax = int(rois[5]) - 1
+    cmin = int(rois[2]) + 1
+    cmax = int(rois[4]) - 1
     r_b = rmax - rmin
     for tt in range(len(border_list)):
         if r_b > border_list[tt] and r_b < border_list[tt + 1]:
@@ -74,22 +55,14 @@ def get_bbox(posecnn_rois, idx = 0, img_width=480, img_length=640):
         cmin -= delt
     return rmin, rmax, cmin, cmax
 
-def preprocessData(img, depth, posecnn_meta, idx = 0, 
+def preprocessData(img, depth, mask, bbox,
                    num_points = 1000, img_width = 480, img_length = 640, 
                    cam_cx = 312.9869, cam_cy = 241.3109,
                    cam_fx = 1066.778, cam_fy = 1067.487,
                    cam_scale = 10000.0):
-    label = np.array(posecnn_meta['labels'])
-    posecnn_rois = np.array(posecnn_meta['rois'])
-    lst = posecnn_rois[:, 1:2].flatten()
-    if(idx > len(lst)):
-        return None
-    itemid = lst[idx]
-
-    rmin, rmax, cmin, cmax = get_bbox(posecnn_rois, idx, img_width, img_length)
-
+    rmin, rmax, cmin, cmax = bbox
     mask_depth = ma.getmaskarray(ma.masked_not_equal(depth, 0))
-    mask_label = ma.getmaskarray(ma.masked_equal(label, itemid))
+    mask_label = ma.getmaskarray(ma.masked_equal(mask, 1))
     mask = mask_label * mask_depth
 
     choose = mask[rmin:rmax, cmin:cmax].flatten().nonzero()[0]
@@ -120,30 +93,23 @@ def preprocessData(img, depth, posecnn_meta, idx = 0,
     cloud = torch.from_numpy(cloud.astype(np.float32))
     choose = torch.LongTensor(choose.astype(np.int32))
     img_masked = norm(torch.from_numpy(img_masked.astype(np.float32)))
-    index = torch.LongTensor([itemid - 1])
 
-    cloud = Variable(cloud).cuda()
-    choose = Variable(choose).cuda()
-    img_masked = Variable(img_masked).cuda()
-    index = Variable(index).cuda()
+    cloud = Variable(cloud)
+    choose = Variable(choose)
+    img_masked = Variable(img_masked)
 
+    if(torch.cuda.is_available()):
+        cloud = cloud.cuda()
+        choose = choose.cuda()
+        img_masked = img_masked.cuda()
+    
     cloud = cloud.view(1, num_points, 3)
     img_masked = img_masked.view(1, 3, img_masked.size()[1], img_masked.size()[2])
 
-    return img_masked, cloud, choose, index
+    return img_masked, cloud, choose
 
 def getCoords(choose, masked_shape):
     return np.unravel_index(to_np(choose), masked_shape[2:])
-
-def getGroundtruth(posecnn_meta, obj_idx):
-    posecnn_idx = posecnn_meta['rois'][obj_idx, 1]
-    pose_idx = np.where(pose_meta['cls_indexes'].flatten()==posecnn_idx)[0][0]
-    mat_gt = pose_meta['poses'][:,:, pose_idx]
-    R_gt = np.eye(4)
-    R_gt[:3,:3] = mat_gt[:3,:3]
-    q_gt = qm.quaternion_from_matrix(R_gt)
-    t_gt = mat_gt[:3,3]
-    return q_gt, t_gt
 
 class DenseFusionEstimator(object):    
     def __init__(self, num_points, num_obj, estimator_weights_file, 
@@ -166,26 +132,32 @@ class DenseFusionEstimator(object):
         else:
             self.refiner = None
     
-    def __call__(self, img, depth, posecnn_meta, idx = 0):
-        data = preprocessData(img, depth, posecnn_meta, idx,
+    def __call__(self, img, depth, mask, bbox, object_label, return_all = False):
+        object_label = Variable(torch.LongTensor([object_label]))
+        if(torch.cuda.is_available()):
+            object_label = object_label.cuda()
+        data = preprocessData(img, depth, mask, bbox,
                               num_points = self.num_points)
         if(data is None):
             return None, None
-        img_masked, cloud, choose, model_index = data
+        img_masked, cloud, choose = data
         max_r, max_t, max_c, pred_r, pred_t, pred_c, emb = self.estimatePose(img_masked, cloud, choose, 
-                model_index, return_all = True)
+                object_label, return_all = True)
         if(self.refiner is not None):
-            refined_r, refined_t = self.refinePose(emb, cloud, model_index, max_r, max_t)
+            refined_r, refined_t = self.refinePose(emb, cloud, object_label, max_r, max_t)
             return refined_r, refined_t
 
+        if(return_all):
+            return max_r, max_t, max_c, pred_r, pred_t, pred_c, emb
+        
         return max_r, max_t
 
-    def globalFeature(self, img_masked, cloud, choose, index):
-        _, _, _, emb = self.estimator(img_masked, cloud, choose, index)
+    def globalFeature(self, img_masked, cloud, choose, object_label):
+        _, _, _, emb = self.estimator(img_masked, cloud, choose, object_label)
         return emb
 
-    def estimatePose(self, img_masked, cloud, choose, index, return_all=True):
-        pred_r, pred_t, pred_c, emb = self.estimator(img_masked, cloud, choose, index)
+    def estimatePose(self, img_masked, cloud, choose, object_label, return_all=True):
+        pred_r, pred_t, pred_c, emb = self.estimator(img_masked, cloud, choose, object_label)
         pred_r = pred_r / torch.norm(pred_r, dim=2).view(1, self.num_points, 1)
         pred_c = pred_c.view(self.bs, self.num_points)
         max_c, which_max = torch.max(pred_c, 1)
@@ -201,7 +173,7 @@ class DenseFusionEstimator(object):
         
         return max_r, max_t, max_c
 
-    def refinePose(self, emb, cloud, index, init_t, init_r, iterations = 2):
+    def refinePose(self, emb, cloud, object_label, init_t, init_r, iterations = 2):
         init_t = init_t.cpu().data.numpy()
         init_r = init_r.cpu().data.numpy()
 
@@ -212,7 +184,7 @@ class DenseFusionEstimator(object):
             init_mat[0:3, 3] = init_t
 
             new_cloud = torch.bmm((cloud - T), R).contiguous()
-            pred_r, pred_t = self.refiner(new_cloud, emb, index)
+            pred_r, pred_t = self.refiner(new_cloud, emb, object_label)
             pred_r = pred_r.view(1, 1, -1)
             pred_r = pred_r / (torch.norm(pred_r, dim=2).view(1, 1, 1))
             
---
+++ lib/evaluate_likelihood.py
@@ -0,0 +1,168 @@
+import numpy as np
+import scipy.io as scio
+from PIL import Image
+
+from tqdm import tqdm
+
+import torch
+from functools import partial
+
+from object_pose_utils.utils import to_np
+from object_pose_utils.utils.interpolation import BinghamInterpolation, binghamNormC
+from object_pose_utils.utils.subrandom import subrandom
+from dense_fusion.data_processing import preprocessPoseCNNMetaData, getYCBGroundtruth
+
+def logSumExp(x, weights, eta):
+    max_x = torch.max(x, dim=1)[0]
+    l_x = max_x + torch.log((weights*torch.exp(x - max_x.unsqueeze(1))).sum(1)) - np.log(eta)
+    return l_x
+
+def evaluateYCBMax(estimator, data_prefix, sigma, return_exponent = False):
+    img = Image.open('{}-color.png'.format(data_prefix))
+    depth = np.array(Image.open('{}-depth.png'.format(data_prefix)))
+    pose_meta = scio.loadmat('{}-meta.mat'.format(data_prefix))
+    posecnn_meta = scio.loadmat('{}-posecnn.mat'.format(data_prefix))
+    object_classes = set(pose_meta['cls_indexes'].flat) & \
+                        set(posecnn_meta['rois'][:,1:2].flatten().astype(int))
+    
+    likelihood = {}
+    for cls_idx in object_classes:
+        obj_idx = np.nonzero(posecnn_meta['rois'][:,1].astype(int) == cls_idx)[0][0]
+        mask, bbox, object_label = preprocessPoseCNNMetaData(posecnn_meta, obj_idx)
+        q_est, t_est = estimator(img, depth, mask, bbox, object_label)
+        q_est = q_est[[1,2,3,0]]
+    
+        bingham_interp = BinghamInterpolation(vertices = [to_np(q_est)], 
+                                             values = torch.Tensor([1]), 
+                                             sigma=sigma)
+    
+        q_gt, _ = getYCBGroundtruth(pose_meta, posecnn_meta, obj_idx)
+        q_gt = torch.Tensor(q_gt).unsqueeze(0)
+        if(torch.cuda.is_available()):
+            q_gt = q_gt.cuda()
+        likelihood[cls_idx] = (bingham_interp(q_gt, return_exponent), bingham_interp.values)
+        #likelihood[cls_idx] = binghamInterp(q_gt)
+    
+    return likelihood
+
+def evaluateYCBEvery(estimator, data_prefix, sigma, return_exponent = False):
+    img = Image.open('{}-color.png'.format(data_prefix))
+    depth = np.array(Image.open('{}-depth.png'.format(data_prefix)))
+    pose_meta = scio.loadmat('{}-meta.mat'.format(data_prefix))
+    posecnn_meta = scio.loadmat('{}-posecnn.mat'.format(data_prefix))
+    object_classes = set(pose_meta['cls_indexes'].flat) & \
+                        set(posecnn_meta['rois'][:,1:2].flatten().astype(int))
+    
+    likelihood = {}
+    for cls_idx in object_classes:
+        obj_idx = np.nonzero(posecnn_meta['rois'][:,1].astype(int) == cls_idx)[0][0]
+        mask, bbox, object_label = preprocessPoseCNNMetaData(posecnn_meta, obj_idx)
+        pred_r, _, pred_c = estimator(img, depth, mask, bbox, object_label, return_all = True)[3:6]
+        pred_r = pred_r[0,:,[1,2,3,0]]
+        bingham_interp = BinghamInterpolation(vertices = to_np(pred_r.detach()), 
+                                              values = pred_c.detach(), 
+                                              sigma = sigma)
+    
+        q_gt, _ = getYCBGroundtruth(pose_meta, posecnn_meta, obj_idx)
+        q_gt = torch.Tensor(q_gt).unsqueeze(0)
+        if(torch.cuda.is_available()):
+            q_gt = q_gt.cuda()
+        likelihood[cls_idx] = (bingham_interp(q_gt, return_exponent), bingham_interp.values)
+    
+    return likelihood
+
+
+def getYCBClassData(dataset_root):
+    with open('{0}/image_sets/classes.txt'.format(dataset_root)) as f:
+        class_list = f.read().split()
+    
+    model_points = {}
+    for cls_id, cls in enumerate(class_list[1:], 1):
+        with open('{0}/models/{1}/points.xyz'.format(dataset_root, cls)) as f:
+            model_points[cls_id] = np.loadtxt(f)
+    
+    return class_list, model_points 
+ 
+
+def evaluateYCBDataset(eval_func, dataset_root, file_list, skip=0):
+    with torch.no_grad():
+        file_prefix = []
+        if(type(file_list) is str):
+            with open(file_list) as f:
+                file_list = f.read().split()
+        elif(type(file_list) not in (tuple, list)):
+            raise TypeError('Invalid file_list type {}. Valid types: str, tuple, list'.format(type(file_list)))
+
+        class_list, model_points = getYCBClassData(dataset_root)
+        num_obj = len(class_list)-1
+
+        likelihoods = {cls_idx:[] for cls_idx in range(1, num_obj+2)}
+        file_list = file_list[::(skip+1)]
+        pbar = tqdm(file_list)
+
+        for data_prefix in pbar:
+            pbar.set_description('Processing {}'.format(data_prefix))
+            lh = eval_func('{0}/{1}'.format(dataset_root, data_prefix))
+            for k,v in lh.items():
+                likelihoods[k].append(v)
+
+        return likelihoods
+
+def subRandomSigmaSearchMax(estimator, dataset_root, file_list, 
+                            sigma_lims = [0, 2*np.pi], 
+                            num_samples = 100):
+    max_likelihood = -np.inf
+    max_sigma = None
+    sigmas = subrandom(num_samples)*(sigma_lims[1]-sigma_lims[0]) + sigma_lims[0]
+    mean_likelihoods = []
+    for j, sigma in enumerate(sigmas):
+        eval_func = partial(evaluateYCBMax, estimator, sigma=sigma, return_exponent=True)
+        likelihoods = evaluateYCBDataset(eval_func, dataset_root, file_list, 20)
+        mean_likelihood = 0
+        n = 0
+        for v in likelihoods.values():
+            l_exp, w = zip(*v)
+            l_exp = torch.cat(l_exp)
+            w = torch.stack(w)
+            mean_likelihood += to_np(torch.sum(logSumExp(l_exp, w, binghamNormC(sigma))))[0]
+            n += len(v)
+        mean_likelihood /= n
+        print("{}: Mean Log Likelihood of Sigma {}: {}".format(j, sigma, mean_likelihood))
+        mean_likelihoods.append(mean_likelihood)
+        if(mean_likelihood > max_likelihood):
+            max_likelihood = mean_likelihood
+            max_sigma = sigma
+            print("Max Sigma after {} samples: {} ({})".format(j+1, sigma, max_likelihood))
+            np.savez('single_max.npz', likelihoods = likelihoods, sigma = max_sigma)
+
+    np.savez('single_max_sigmas.npz', mean_likelihoods = np.array(mean_likelihoods), sigmas = sigmas)
+
+def subRandomSigmaSearchEvery(estimator, dataset_root, file_list, 
+                              sigma_lims = [0, 100], 
+                              num_samples = 100):
+    max_likelihood = -np.inf
+    max_sigma = None
+    sigmas = subrandom(num_samples)*(sigma_lims[1]-sigma_lims[0]) + sigma_lims[0]
+    mean_likelihoods = []
+    for j, sigma in enumerate(sigmas):
+        eval_func = partial(evaluateYCBEvery, estimator, sigma=sigma)
+        likelihoods = evaluateYCBDataset(eval_func, dataset_root, file_list, 20)
+        mean_likelihood = 0
+        n = 0
+        for v in likelihoods.values():
+            l_exp, w = zip(*v)
+            l_exp = torch.cat(l_exp)
+            w = torch.stack(w)
+            mean_likelihood += to_np(torch.sum(logSumExp(l_exp, w, binghamNormC(sigma))))[0]
+            n += len(v)
+        mean_likelihood /= n
+        print("{}: Mean Log Likelihood of Sigma {}: {}".format(j, sigma, mean_likelihood))
+        mean_likelihoods.append(mean_likelihood)
+        if(mean_likelihood > max_likelihood):
+            max_likelihood = mean_likelihood
+            max_sigma = sigma
+            print("Max Sigma after {} samples: {} ({})".format(j+1, sigma, max_likelihood))
+            np.savez('bingham_every.npz', likelihoods = likelihoods, sigma = max_sigma)
+
+    np.savez('bingham_every_sigmas.npz', mean_likelihoods = np.array(mean_likelihoods), sigmas = sigmas)
+
