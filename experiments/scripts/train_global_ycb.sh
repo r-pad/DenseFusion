040491d102b86a38f991a3de50b588f666d83802
Brian Okorn <bokorn@andrew.cmu.edu>
2019-05-30 11:44:43 -0400

    Added global featreu trainign script

---
+++ experiments/scripts/train_global_ycb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -x
+set -e
+
+export PYTHONUNBUFFERED="True"
+export CUDA_VISIBLE_DEVICES=0
+
+python3 ./tools/train_global.py --dataset ycb\
+  --dataset_root ./datasets/ycb/YCB_Video_Dataset\
+  --resume_posenet pose_model_current.pth
